961b4a98e9ce34227c86ac180ca472fc0f342fc3
Joshua Ollendick <joshua.ollendick@gmail.com>
2018-06-17 15:32:17 -0500

    implement GuessCount component tests

--- src/components/guess-count.test.js
+++ src/components/guess-count.test.js
@@ -8,4 +8,10 @@ describe('<GuessCount />', () => {
     it('Renders without crashing', () => {
         shallow(<GuessCount />);
     });
+
+    it('Should render guess count feedback', () => {
+        const value = 6;
+        const wrapper = shallow(<GuessCount guessCount={value}/>);
+        expect(wrapper.text()).toEqual(`You've made ${value} guesses!`);
+    });
 });
