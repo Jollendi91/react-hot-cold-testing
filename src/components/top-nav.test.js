01ca1ded74ed411b8da7dc3d800ede7919f614af
Joshua Ollendick <joshua.ollendick@gmail.com>
2018-06-17 16:40:41 -0500

    implement TopNav component tests

--- src/components/top-nav.test.js
+++ src/components/top-nav.test.js
@@ -8,4 +8,20 @@ describe('<TopNav />', () => {
     it('Renders without crashing', () => {
         shallow(<TopNav />);
     });
+
+    it('Should run the onRestartGame callback when clicked', () => {
+        const callback = jest.fn();
+        const wrapper = shallow(<TopNav onRestartGame={callback} />);
+        const link = wrapper.find('.new');
+        link.simulate('click');
+        expect(callback).toHaveBeenCalled();
+    });
+
+    it('Should provide an Aurual Update when on screen reader', () => {
+        const callback = jest.fn();
+        const wrapper = shallow(<TopNav onGenerateAuralUpdate={callback} />);
+        const link = wrapper.find('.status-link');
+        link.simulate('click');
+        expect(callback).toHaveBeenCalled();
+    });
 });
